b0629abd88b90299ace3c85a4ae3ac8a241679df
dev4vasu13 <sirivellavasu5@gmail.com>
2025-07-03 11:25:58 +0530

    fix: switch from BrowserRouter to HashRouter for better routing support feat: add 404.html for handling page not found redirects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Components/authContext';
 import { CartProvider } from './Components/CartContext';
 import Navbar from './Components/Navbar';
